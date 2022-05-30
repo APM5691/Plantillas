742cbe0aad43527e6a9646fbb0630ee694b659bd
221910938 <al221910938@gmail.com>
2022-05-29 19:23:08 -0500

    modal

---
+++ Modal/script.js
@@ -0,0 +1,8 @@
+function modal() {
+  var modal = document.getElementById("openModal");
+  if (modal.style.display == "block") {
+    modal.style.display = "none";
+  } else {
+    modal.style.display = "block";
+  }
+}
